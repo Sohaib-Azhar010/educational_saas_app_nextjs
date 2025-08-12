f6be86391cb98ae539cb11c13dd8fe03642a3045
Sohaib Azhar <sohaibazhar04@gmail.com>
2025-08-12 13:40:03 +0500

    clerk billing done

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { PricingTable } from "@clerk/nextjs";
+import React from "react";
 
 const Subscription = () => {
   return (
-    <div>
-      Subscription
-    </div>
-  )
-}
+    <>
+      <div className="container flex items-center justify-center mx-auto mt-5">
+        <h1>Subscription Plans</h1>
+      </div>
+      <div className="container flex items-center justify-center mx-auto mt-10 px-5">
+        <PricingTable />
+      </div>
+    </>
+  );
+};
 
-export default Subscription
+export default Subscription;
